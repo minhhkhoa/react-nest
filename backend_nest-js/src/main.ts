aff259e8c915f84c69558c05cccae4141202fc8f
Khoa <khoanguyen.1881@gmail.com>
2025-06-16 17:23:04 +0700

    add interceptor for success request
    - Vào notion đọc.

--- backend_nest-js/src/main.ts
+++ backend_nest-js/src/main.ts
@@ -3,15 +3,20 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  //- use global interceptor
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   //- use global JwtAuthGuard
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector)); //- check xem có gửi token kèm theo không
 
+  //- use global pipe
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // ép kiểu theo DTO
