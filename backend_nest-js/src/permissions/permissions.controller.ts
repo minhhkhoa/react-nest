ca0913a5510266297251e538932c51b6e0741ace
Khoa <khoanguyen.1881@gmail.com>
2025-07-03 14:40:53 +0700

    success feature create + update permission
    - Chú ý khi thêm mới và cập nhật thì cần kiểm tra xem apiPath + method đó đã tồn tại hay chưa, ta chỉ chấp nhận nếu 1 apiPath + method(permission) là duy nhất.
    Ví dụ việc create User thì chỉ có 1 cái api để làm việc đó thôi.

--- backend_nest-js/src/permissions/permissions.controller.ts
+++ backend_nest-js/src/permissions/permissions.controller.ts
@@ -1,15 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { IUser } from 'src/users/users.interface';
+import { ResponseMessage, userDecorator } from 'src/decorator/customize';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Post()
-  create(@Body() createPermissionDto: CreatePermissionDto) {
-    return this.permissionsService.create(createPermissionDto);
+  @ResponseMessage('Create a new permission!')
+  create(
+    @Body() createPermissionDto: CreatePermissionDto,
+    @userDecorator() user: IUser,
+  ) {
+    return this.permissionsService.create(createPermissionDto, user);
   }
 
   @Get()
@@ -19,16 +33,21 @@ export class PermissionsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
+    return this.permissionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  @ResponseMessage('Update a permission!')
+  update(
+    @Param('id') id: string,
+    @Body() updatePermissionDto: UpdatePermissionDto,
+    @userDecorator() user: IUser
+  ) {
+    return this.permissionsService.update(id, updatePermissionDto, user);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+    return this.permissionsService.remove(id);
   }
 }
--- backend_nest-js/src/permissions/permissions.service.ts
+++ backend_nest-js/src/permissions/permissions.service.ts
@@ -1,26 +1,110 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Permission, PermissionDocument } from './schemas/permission.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { BadRequestCustom } from 'src/customExceptions/BadRequestCustom';
+import mongoose, { Types } from 'mongoose';
 
 @Injectable()
 export class PermissionsService {
-  create(createPermissionDto: CreatePermissionDto) {
-    return 'This action adds a new permission';
+  constructor(
+    @InjectModel(Permission.name)
+    private permissionModel: SoftDeleteModel<PermissionDocument>,
+  ) {}
+  async create(dto: CreatePermissionDto, user: IUser) {
+    const { apiPath, method, name, module } = dto;
+
+    // 1. Check tồn tại (bao gồm cả bản đã soft-delete)
+    const existing = await this.permissionModel
+      .findOne({ apiPath, method })
+      .exec();
+    if (existing) {
+      throw new ConflictException(
+        `Permission cho [${method} ${apiPath}] đã tồn tại`,
+      );
+    }
+
+    // 2. Tạo mới
+    try {
+      const permission = await this.permissionModel.create({
+        name,
+        apiPath,
+        method,
+        module,
+        createdBy: {
+          _id: user._id,
+          email: user.email,
+          name: user.name,
+        },
+      });
+      return {
+        _id: permission._id,
+        createdAt: permission.createdAt,
+      };
+    } catch (err) {
+      throw new BadRequestCustom(err.message, !!err.message);
+    }
   }
 
   findAll() {
     return `This action returns all permissions`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} permission`;
   }
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
+  async update(
+    id: string,
+    updatePermissionDto: UpdatePermissionDto,
+    user: IUser,
+  ) {
+    const { apiPath, method, name, module } = updatePermissionDto;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestCustom('Id Permission không hợp lệ!', !!id);
+    }
+
+    const checkExistPermission = await this.permissionModel.findById(id);
+    if (!checkExistPermission) {
+      throw new BadRequestCustom(`Không tìm thấy Permission với id ${id}`);
+    }
+
+    const conflict = await this.permissionModel
+      .findOne({
+        apiPath,
+        method,
+        _id: { $ne: new Types.ObjectId(id) }, //- bỏ đứa đang sửa đi
+      })
+      .exec();
+    if (conflict) {
+      throw new ConflictException(
+        `Permission cho [${method} ${apiPath}] đã tồn tại`,
+      );
+    }
+
+    try {
+      const filter = { _id: id };
+      const update = {
+        $set: {
+          ...updatePermissionDto,
+          updatedBy: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+          },
+        },
+      };
+
+      const result = await this.permissionModel.updateOne(filter, update);
+
+      return result;
+    } catch (error) {}
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} permission`;
   }
 }
