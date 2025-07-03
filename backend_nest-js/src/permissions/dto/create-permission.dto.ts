4c5c49f7ffb9b8c457bc9ac046bc70f21def3392
Khoa <khoanguyen.1881@gmail.com>
2025-07-03 13:40:36 +0700

    success shceme + dto for module Permission

--- backend_nest-js/src/permissions/dto/create-permission.dto.ts
+++ backend_nest-js/src/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePermissionDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreatePermissionDto {
+  @IsNotEmpty({ message: 'Tên quyền không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'apiPath quyền không được để trống' })
+  apiPath: string;
+
+  @IsNotEmpty({ message: 'Method quyền không được để trống' })
+  method: string;
+
+  @IsNotEmpty({ message: 'Module quyền không được để trống' })
+  module: string;
+}
--- backend_nest-js/src/permissions/schemas/permission.schema.ts
+++ backend_nest-js/src/permissions/schemas/permission.schema.ts
@@ -1,1 +1,54 @@
-export class Permission {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type PermissionDocument = HydratedDocument<Permission>;
+
+@Schema({ timestamps: true })
+export class Permission {
+  @Prop()
+  name: string;
+
+  @Prop()
+  apiPath: string;
+
+  @Prop()
+  method: string;
+
+  @Prop()
+  module: string; //- cái này để biết quyền này là của module nào vd: của users, companies, ...
+
+  @Prop()
+  createdAt?: Date;
+
+  @Prop()
+  updatedAt?: Date;
+
+  @Prop()
+  deletedAt?: Date;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+    email: string;
+  };
+
+  @Prop()
+  isDeleted?: boolean;
+}
+
+export const PermissionSchema = SchemaFactory.createForClass(Permission); //- tạo ra một schema từ class Permission
